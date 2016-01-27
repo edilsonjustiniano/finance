ed72092d993772d9db67a0c13b1cd7c93d07d3a2
Edilson Justiniano <edilsonjustiniano@Edilsons-MacBook-Pro.local>
2016-01-27 12:55:36 -0200

    Include some informations to payment list page

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,17 @@
 angular.module('FinanceApp', ['ngRoute'])
-    .config(function($routeProvider) {
-        $routeProvider.when('/payments', {
-            templateUrl: 'views/paymentList.html',
-            controller: 'PaymentListController'
-        }).
-        when('/payment/:idContato', {
-            templateUrl: 'views/payment.html',
-            controller: 'PaymentController'
-        }).
-        when('/payment', {
-            templateUrl: 'views/payment.html',
-            controller: 'PaymentController'
-        });
-        $routeProvider.otherwise({redirectTo: '/payments'});
-    }
+.config(function($routeProvider) {
+    $routeProvider.when('/payments', {
+        templateUrl: 'views/paymentList.html',
+        controller: 'PaymentListController'
+    }).
+    when('/payment/:idContato', {
+        templateUrl: 'views/payment.html',
+        controller: 'PaymentController'
+    }).
+    when('/payment', {
+        templateUrl: 'views/payment.html',
+        controller: 'PaymentController'
+    });
+    $routeProvider.otherwise({redirectTo: '/payments'});
+}
 );
